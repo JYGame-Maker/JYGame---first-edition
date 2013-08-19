32be8f0487ae2179cf74809fb253ae2fe83d1509
JYGame-Maker <jygame-cpp@outlook.com>
2013-08-19 14:31:42 +0800

    Create mainFlie.cpp

---
+++ mainFlie.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "mainH.h"
+//金庸江湖，一个武侠游戏，控制端版的，这个游戏将写三个版本，新手写代码，可能不规范，见谅啊
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+    cout<<"欢迎来到金庸江湖，一个控制端游戏"<<endl;
+}
